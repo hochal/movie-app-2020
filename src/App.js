2e07ae2b22e6e6f7a073024ff000c6c3901ecae9
양호철 <748868@gmail.com>
2020-10-21 22:05:16 +0900

    almost done!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from "./routes/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <HashRouter>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+    </HashRouter>
+);
 
 export default App;
